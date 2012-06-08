75b7688607e196e45a9165b8480a7b17714a2536
Roger Binns <rogerb@rogerbinns.com>
2012-06-07 21:05:42 -0700

    Extensive changes to match Play Store behaviour and more useful

--- src/com/giago/referraltester/ReferralTester.java
+++ src/com/giago/referraltester/ReferralTester.java
@@ -1,5 +1,6 @@
 package com.giago.referraltester;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.ActivityNotFoundException;
@@ -12,53 +13,52 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class ReferralTester extends BaseActivity {
-	
+public class ReferralTester extends Activity {
+
 	private static final String ARTICLE = "http://www.dev-articles.com/article/Android-Analytics-referral-tracking-447001";
-	
+
 	private static final int MISSING_SCANNER_DIALOG_ID = 10000;
-	
+
 	public static final String DEFAULT_TITLE = "Install Barcode Scanner?";
 	public static final String DEFAULT_MESSAGE = "This application requires Barcode Scanner. Would you like to install it?";
 	public static final String DEFAULT_YES = "Yes";
 	public static final String DEFAULT_NO = "No";
 	private static final String PACKAGE = "com.google.zxing.client.android";
-	
+
 	private Button manual;
 	private String referralUrl;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.referral_tester_activity);
-    }
-    
-    @Override
-    protected void onResume() {
-    	super.onResume();
-    	trackReferralTesterActivity();
-        Button scan = (Button)findViewById(R.id.scan_url_btn);
-        scan.setOnClickListener(new OnClickListener() {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.referral_tester_activity);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Button scan = (Button)findViewById(R.id.scan_url_btn);
+		scan.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-		        intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
-		        try {
-		        	startActivityForResult(intent, 0);
-		        } catch (ActivityNotFoundException e) {
+				intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+				try {
+					startActivityForResult(intent, 0);
+				} catch (ActivityNotFoundException e) {
 					showDialog(MISSING_SCANNER_DIALOG_ID);
 				}
 			}
 		});
-        manual = (Button)findViewById(R.id.manual_url_btn);
-        manual.setOnClickListener(new OnClickListener() {
+		manual = (Button)findViewById(R.id.manual_url_btn);
+		manual.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				ReferralTester.this.startActivity(Send.getIntent(ReferralTester.this));
 			}
 		});
-        Button readMore = (Button)findViewById(R.id.read_more_btn);
-        readMore.setOnClickListener(new OnClickListener() {
+		Button readMore = (Button)findViewById(R.id.read_more_btn);
+		readMore.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				Intent i = new Intent(Intent.ACTION_VIEW);
@@ -66,48 +66,48 @@ public class ReferralTester extends BaseActivity {
 				startActivity(i);
 			}
 		});
-    }
-    
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        if (requestCode == 0) {
-            if (resultCode == RESULT_OK) {
-            	referralUrl = intent.getStringExtra("SCAN_RESULT");            	
-            	startActivity(Send.getIntent(ReferralTester.this, referralUrl));                 
-            } else {
-            	Toast.makeText(ReferralTester.this, "No data from scan, try manual url generation", Toast.LENGTH_LONG).show();
-            }
-        }
-    }
-    
-    @Override
-    protected Dialog onCreateDialog(int id) {
-        Dialog dialog = null;
-        switch (id) {
-            case MISSING_SCANNER_DIALOG_ID:
-                dialog = new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_alert)
-				          .setTitle(DEFAULT_TITLE)
-				          .setMessage(DEFAULT_MESSAGE)
-				          .setNegativeButton(DEFAULT_NO, new DialogInterface.OnClickListener() {
-				        	  @Override
-				        	  public void onClick(DialogInterface dialog, int which) {
-				        		  dialog.dismiss();
-				        	  }
-				          })
-				          .setPositiveButton(DEFAULT_YES, new DialogInterface.OnClickListener() {
-								@Override
-								public void onClick(DialogInterface dialog, int which) {
-									Uri uri = Uri.parse("market://search?q=pname:" + PACKAGE);
-							        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-							        ReferralTester.this.startActivity(intent);
-								}
-				          }).create();
-                break;
-        }
-        if (dialog == null) {
-            dialog = super.onCreateDialog(id);
-        }
-        return dialog;
-    }
-        
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if (requestCode == 0) {
+			if (resultCode == RESULT_OK) {
+				referralUrl = intent.getStringExtra("SCAN_RESULT");
+				startActivity(Send.getIntent(ReferralTester.this, referralUrl));
+			} else {
+				Toast.makeText(ReferralTester.this, "No data from scan, try manual url generation", Toast.LENGTH_LONG).show();
+			}
+		}
+	}
+
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		Dialog dialog = null;
+		switch (id) {
+		case MISSING_SCANNER_DIALOG_ID:
+			dialog = new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_alert)
+			.setTitle(DEFAULT_TITLE)
+			.setMessage(DEFAULT_MESSAGE)
+			.setNegativeButton(DEFAULT_NO, new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();
+				}
+			})
+			.setPositiveButton(DEFAULT_YES, new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					Uri uri = Uri.parse("market://search?q=pname:" + PACKAGE);
+					Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+					ReferralTester.this.startActivity(intent);
+				}
+			}).create();
+			break;
+		}
+		if (dialog == null) {
+			dialog = super.onCreateDialog(id);
+		}
+		return dialog;
+	}
+
 }
--- src/com/giago/referraltester/Send.java
+++ src/com/giago/referraltester/Send.java
@@ -1,22 +1,34 @@
 package com.giago.referraltester;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class Send extends BaseActivity {
-	
+public class Send extends Activity {
+
 	private static final String REFERRAL_URL_EXTRA = "referralUrl";
 	private Button send;
 	private EditText packageName;
@@ -26,151 +38,262 @@ public class Send extends BaseActivity {
 	private EditText campaignContent;
 	private EditText campaignName;
 	private Map<String, String> properties;
-	
+	// put into logcat so we can work out where events start
+	private String basemarker="!RefTestMarker!";
+	private String marker="";
+	private SharedPreferences mSharedPreferences;
+	private LogReader mLogReader;
+
 	public static final Intent getIntent(Context context, String  referralUrl) {
 		Intent i = getIntent(context);
 		i.putExtra(REFERRAL_URL_EXTRA, referralUrl);
 		return i;
 	}
-	
+
 	private static final String getReferralUrl(Intent i) {
 		return i.getStringExtra(REFERRAL_URL_EXTRA);
 	}
-	
+
 	public static final Intent getIntent(Context context) {
 		return new Intent(context, Send.class);
 	}
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.send_activity);
-        trackSendActivity();
-        
-        packageName = (EditText)findViewById(R.id.package_name);
-        campaignSource = (EditText)findViewById(R.id.campaign_source);
-        campaignMedium = (EditText)findViewById(R.id.campaign_medium);
-        campaignTerm = (EditText)findViewById(R.id.campaign_term);
-        campaignContent = (EditText)findViewById(R.id.campaign_content);
-        campaignName = (EditText)findViewById(R.id.campaign_name);
-        
-        String referralUrl = getReferralUrl(getIntent());
-    	if(!TextUtils.isEmpty(referralUrl)) {
-    		String referrer = getReferrer(referralUrl);
-    		properties = getMap(referrer);
-    		String id = getId(referralUrl);
-    		if(!TextUtils.isEmpty(id)) {
-    			properties.put("id", id);
-    		}
-    		setValue(packageName, "id");
-    		setValue(campaignSource, "utm_source");
-    		setValue(campaignMedium, "utm_medium");
-    		setValue(campaignTerm, "utm_term");
-    		setValue(campaignContent, "utm_content");
-    		setValue(campaignName, "utm_campaign");
-    	}
-    	
-    	send = (Button)findViewById(R.id.send);
-    	send.setOnClickListener(new OnClickListener() {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mSharedPreferences=PreferenceManager.getDefaultSharedPreferences(this);
+
+		setContentView(R.layout.send_activity);
+
+		packageName = (EditText)findViewById(R.id.package_name);
+		campaignSource = (EditText)findViewById(R.id.campaign_source);
+		campaignMedium = (EditText)findViewById(R.id.campaign_medium);
+		campaignTerm = (EditText)findViewById(R.id.campaign_term);
+		campaignContent = (EditText)findViewById(R.id.campaign_content);
+		campaignName = (EditText)findViewById(R.id.campaign_name);
+
+		String referralUrl = getReferralUrl(getIntent());
+		if(!TextUtils.isEmpty(referralUrl)) {
+			String referrer = getReferrer(referralUrl);
+			properties = getMap(referrer);
+			String id = getId(referralUrl);
+			if(!TextUtils.isEmpty(id)) {
+				properties.put("id", id);
+			}
+			setValue(packageName, "id");
+			setValue(campaignSource, "utm_source");
+			setValue(campaignMedium, "utm_medium");
+			setValue(campaignTerm, "utm_term");
+			setValue(campaignContent, "utm_content");
+			setValue(campaignName, "utm_campaign");
+		} else {
+			packageName.setText(mSharedPreferences.getString("id", ""));
+			campaignSource.setText(mSharedPreferences.getString("utm_source", ""));
+			campaignMedium.setText(mSharedPreferences.getString("utm_medium", ""));
+			campaignTerm.setText(mSharedPreferences.getString("utm_term", ""));
+			campaignContent.setText(mSharedPreferences.getString("utm_content", ""));
+			campaignName.setText(mSharedPreferences.getString("utm_campaign", ""));
+		}
+
+		send = (Button)findViewById(R.id.send);
+		send.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				sendBroadcastIntent();
 			}
 		});
-    	
-    }
-    
-    private void sendBroadcastIntent() {
-    	Intent i = new Intent("com.android.vending.INSTALL_REFERRER");
-    	String id = getId();
-    	if(checkId(id)) {
-    		return;
-    	}
-    	i.setPackage(id);
-    	String referrer = generateReferrer();
-    	if(checkfield(referrer, "utm_source")) {
-    		return;
-    	}
-    	if(checkfield(referrer, "utm_medium")) {
-    		return;
-    	}
-    	if(checkfield(referrer, "utm_campaign")) {
-    		return;
-    	}
-    	i.putExtra("referrer", referrer);
-    	sendBroadcast(i);
-    }
-    
-    private boolean checkfield(String referrer, String field) {
-    	if(!referrer.contains(field)) {
-    		Toast.makeText(this, "The field " + field + " is mandatory", Toast.LENGTH_LONG).show();
-    		return true;
-    	}
-    	return false;
-    }
-    
-    private boolean checkId(String id) {
-    	if(TextUtils.isEmpty(id)) {
-    		Toast.makeText(this, "The field package name is mandatory", Toast.LENGTH_LONG).show();
-    		return true;
-    	}
-    	return false;
-    }
-    
-    private String getId() {
-    	return packageName.getText().toString();
-    }
-    
-    private String generateReferrer() {
-    	StringBuilder sb = new StringBuilder();
-    	
-    	append(sb, campaignSource, "utm_source");
-    	sb.append("&");
-    	append(sb, campaignMedium, "utm_medium");
-    	sb.append("&");
-    	if(append(sb, campaignTerm, "utm_term")) {    		
-    		sb.append("&");
-    	}
-    	if(append(sb, campaignContent, "utm_content")) {
-    		sb.append("&");
-    	}
-    	append(sb, campaignName, "utm_campaign");
+
+	}
+
+	private void sendBroadcastIntent() {
+		Intent i = new Intent("com.android.vending.INSTALL_REFERRER");
+		String id = getId();
+		if(checkId(id))
+			return;
+
+		if (mLogReader!=null) {
+			mLogReader.cancel(true);
+			mLogReader=null;
+		}
+
+		// save values
+		mSharedPreferences.edit()
+		.putString("id", packageName.getText().toString())
+		.putString("utm_source", campaignSource.getText().toString())
+		.putString("utm_medium", campaignMedium.getText().toString())
+		.putString("utm_term", campaignTerm.getText().toString())
+		.putString("utm_content", campaignContent.getText().toString())
+		.putString("utm_campaign", campaignName.getText().toString())
+		.commit();
+
+		i.setPackage(id);
+		String referrer = generateReferrer();
+		i.putExtra("referrer", referrer);
+
+		TextView tv=(TextView)findViewById(R.id.status_info);
+		tv.setText("Referrer string is "+referrer+"\n");
+		PackageManager pm=getPackageManager();
+		List<ResolveInfo> receivers=pm.queryBroadcastReceivers(i, 0);
+		if(receivers==null || receivers.size()==0) {
+			tv.append(">>> No broadcast receivers found! Nothing is likely to happen\n");
+		} else {
+			tv.append(String.format("%d receivers found.", receivers.size()));
+			for(ResolveInfo ri : receivers) {
+				tv.append(String.format("Package: %s  Class: %s  Exported: %s  Enabled: %s\n", ri.activityInfo.packageName,
+						ri.activityInfo.name, ri.activityInfo.exported, ri.activityInfo.enabled));
+			}
+			if(receivers.size()>1) {
+				tv.append(" Only the first will be called to match Market/Store/Finsky behaviour.");
+				i.setClassName(receivers.get(0).activityInfo.packageName,
+						receivers.get(0).activityInfo.name);
+			}
+			tv.append("\n");
+		}
+		// yes it is a lie but any further along means race conditions
+		tv.append("Broadcast sent - monitoring logcat\n\n");
+		marker=basemarker+Long.toHexString((System.currentTimeMillis()/100)%999983);
+		mLogReader=new LogReader(tv);
+		mLogReader.execute();
+		Log.d(marker, marker);
+		sendBroadcast(i);
+	}
+
+	private class LogReader extends AsyncTask<Void, String, Void> {
+
+		LogReader(TextView tv) {
+			textview=tv;
+		}
+
+		TextView textview;
+
+		// various irrelevant noise that appears in logcat
+		final String[] gunk={
+				"Audio", "WifiStateMachine", "HierarchicalStateMachine", "ProvisionLog",
+				"InputReader", "InputDispatcher", "Keyguard", "SurfaceFlinger",
+				"CLIPBOARD", "BounceScrollRunnableDefault", "alsa_", "WindowOrientationListener",
+				"Power", "TabletStatus", "Wifi", "TODmobile", "DigitalClock", "BatteryService"
+		};
+		protected Void doInBackground(Void... params) {
+			Process process;
+			try {
+				process = Runtime.getRuntime().exec("logcat");
+			} catch (IOException e) {
+				publishProgress(">>> Failed to execute logcat: "+Log.getStackTraceString(e)+"\n");
+				return null;
+			}
+			long start=0;
+			BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			String line;
+			boolean seenmarker=false;
+			try {
+				readline:
+					while ((line = br.readLine()) != null) {
+						if(isCancelled()) {
+							break;
+						}
+						if(!seenmarker) {
+							if(line.contains(marker)) {
+								seenmarker=true;
+								start=System.currentTimeMillis();
+							}
+							continue;
+						}
+						if(line.startsWith("D/dalvik")) {
+							continue;
+						}
+						// filter out a whole lot of junk messages, especially ones about input being sent to
+						// our text view
+						for(String g: gunk) {
+							if(line.substring(2).startsWith(g)) {
+								continue readline;
+							}
+						}
+						publishProgress(line+"\n");
+						if(System.currentTimeMillis()-start>10000) {
+							publishProgress("10 seconds have elapsed\n");
+							break;
+						}
+					}
+			} catch (IOException e) {
+				if(!isCancelled()) {
+					publishProgress(">>> Reading from logcat failed: "+Log.getStackTraceString(e)+"\n");
+				}
+			}
+			process.destroy();
+			if(!isCancelled()) {
+				publishProgress(">>> Stopping logcat\n");
+			}
+			return null;
+		}
+
+		@Override
+		protected void onProgressUpdate(String ...progress) {
+			textview.append(progress[0]);
+		}
+
+		protected void onPostExecute(Void...voids) {
+			textview.append(">>> Done\n");
+		}
+	}
+
+	private boolean checkId(String id) {
+		if(TextUtils.isEmpty(id)) {
+			Toast.makeText(this, "The field package name is mandatory", Toast.LENGTH_LONG).show();
+			return true;
+		}
+		return false;
+	}
+
+	private String getId() {
+		return packageName.getText().toString();
+	}
+
+	private String generateReferrer() {
+		StringBuilder sb = new StringBuilder();
+
+		append(sb, campaignSource, "utm_source");
+		append(sb, campaignMedium, "utm_medium");
+		append(sb, campaignTerm, "utm_term");
+		append(sb, campaignContent, "utm_content");
+		append(sb, campaignName, "utm_campaign");
 		return sb.toString();
 	}
-    
-    private boolean append(StringBuilder sb, EditText edit, String parameter) {
-    	String text = edit.getText().toString();
-    	if(!TextUtils.isEmpty(text)) {
-    		sb.append(parameter).append("=").append(text);
-    		return true;
-    	}
-    	return false;
-    }
+
+	private void append(StringBuilder sb, EditText edit, String parameter) {
+		String text = edit.getText().toString();
+		if(text.length()>0) {
+			if(sb.length()>0) {
+				sb.append("&");
+			}
+			sb.append(parameter).append("=").append(text);
+		}
+	}
 
 	private void setValue(EditText t, String key) {
-    	if(properties.containsKey(key)) {
+		if(properties.containsKey(key)) {
 			t.setText(properties.get(key));
-		}	
-    }
-    
-    @Override
-    protected void onResume() {
-    	super.onResume();
-    }
-    
-    private Map<String, String> getMap(String paramString) {
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	private Map<String, String> getMap(String paramString) {
 		String[] arrayOfString1 = paramString.split("&");
 		HashMap<String, String> localHashMap = new HashMap<String, String>();
 		for (String pair : arrayOfString1) {
 			String[] entry = pair.split("=");
-			if (entry.length != 2)
+			if (entry.length != 2) {
 				break;
+			}
 			localHashMap.put(entry[0], entry[1]);
 		}
 		return localHashMap;
 	}
 
-    private String getReferrer(String url) {
+	private String getReferrer(String url) {
 		try {
 			URI uri = new URI(url);
 			String referrer = uri.getQuery().split("referrer=")[1];
@@ -179,19 +302,19 @@ public class Send extends BaseActivity {
 			return null;
 		}
 	}
-    
+
 	private String getId(String url) {
 		try {
 			URI uri = new URI(url);
 			String query = uri.getQuery();
-			
+
 			String[] arrayOfString1 = query.split("&");
 			String id = arrayOfString1[0].split("=")[1];
-			
+
 			return id;
 		} catch (Exception e) {
 			return null;
 		}
 	}
-    
+
 }
